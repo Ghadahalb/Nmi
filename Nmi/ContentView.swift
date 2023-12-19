d16bcffe9aeb54c7a1ae2355865ade28f4ee17f6
GhadahAlb <ghadah.b13@gmail.com>
2023-12-19 09:16:05 +0300

    Initial Commit

---
+++ Nmi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nmi
+//
+//  Created by Ghadah Albassam on 19/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nmi/NmiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NmiApp.swift
+//  Nmi
+//
+//  Created by Ghadah Albassam on 19/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct NmiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
